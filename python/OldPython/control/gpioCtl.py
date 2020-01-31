66f1ac450551161fcb8f15d063b8f33edc8562ae
root <root@DESKTOP-CQH9QGF.localdomain>
2020-01-31 22:04:44 +0800

    upload

--- python/OldPython/control/gpioCtl.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-
-import RPi.GPIO as GPIO
-import time
-
-def setBuz(channel, time = 1):
-    GPIO.output(channel, GPIO.HIGH)
-    time.sleep(time)
-    GPIO.output(channel, GPIO.LOW)
--- python/ctl/statusChk.py
+++ python/ctl/statusChk.py
@@ -12,28 +12,31 @@ class sensor(threading.Thread):
         threading.Thread.__init__(self)
         self._uid = uid
         self._name = 'NONE'
+        self._sleepTime = 1
+        conn = sqlite3.connect(sys.path[0] + '/' + ctl.getGlobalVar('config')['Common']['DB']).cursor()
+        self._name, self._lowVar, self._highVar, self._mtd, self._alarmMode, self._useEmail, self._useBuz = self.getlVar(conn)
         pass
 
     def getlVar(self, cursor):
         # ctl.getGlobalVar('sensorData')['temperature']
-        cursor.execute("SELECT name, lowVar, highVar, method, alarmMode from sensor WHERE uid = ?", str(self._uid))
+        cursor.execute("SELECT name, lowVar, highVar, method, alarmMode, useEmail, useBuz from sensor WHERE uid = ?", str(self._uid))
         for row in cursor:
-            return row[0], row[1], row[2], row[3], row[4]
+            return row[0], row[1], row[2], row[3], row[4], row[5], row[6]
 
-    def alarm(self, status, alarmMode = 0):
-        if status == -1:
+    def alarm(self):
+        if self._status == -1:
             print('status:' + self._name + " is not installed")
-        elif status == 0:
+        elif self._status == 0:
             print('status:' + self._name + " is safe")
-        elif status == 1:
-            print('status:' + self._name + " is Warning")
-        elif status == 2:
-            print('status:' + self._name + " is Email")
+        elif self._status == 1:
+            print('status:' + self._name + " is danger")
+        elif self._status == 2:
+            print('status:' + self._name + " is warning")
         # status
         ### -1:err 0:safe 1:edge safe(yellow)  2.mail warning 3:buz warning(red)
         pass
 
-    def watchVar(self, name, lowVar, highVar, mtd, alarmMode = 0,sleepTime = 1):
+    def watchVar(self):
         #method: 
         ### 0 : low < data > high red alar
         ### 1 : data > low > high
@@ -42,42 +45,42 @@ class sensor(threading.Thread):
         ### 0:NORMAL
         ### 1:BUZ
         #'flag' + name.capitalize():-1 gray 0:green 1:yellow 2:red
-        self._msg = 'Sensor [' + name + '] watch thread started.'
+        self._msg = 'Sensor [' + self._name + '] watch thread started.'
         LOG.info(self._msg)
         print(self._msg)
         while True:
             if ctl.getGlobalVar('sensorData') != False:
-                nowVar = ctl.getGlobalVar('sensorData')[name]
+                nowVar = ctl.getGlobalVar('sensorData')[self._name]
                 # print(nowVar)
                 # time.sleep(sleepTime)
                 # ctl.setGlobalVar('flag' + name.capitalize() , 1)
                 ### 1 : low < data > high
                 if mtd == 0 :
                     if 0 < nowVar < lowVar:
-                        status = 3
+                        self._status = 3
                     elif nowVar > highVar:
-                        status = 3
+                        self._status = 3
                     elif nowVar == -1:
-                        status = -1
+                        self._status = -1
                     else:
-                        status = 0
+                        self._status = 0
                 elif mtd == 1:
                     if nowVar >= highVar:
-                        status = 2
+                        self._status = 2
                     elif nowVar >= lowVar:
-                        status = 1
+                        self._status = 1
                     elif nowVar == -1:
-                        status = -1
+                        self._status = -1
                     else:
-                        status = 0
-                ctl.setFlagVar(name, status)
-                self.alarm(status)
+                        self._status = 0
+                ctl.setFlagVar(self._name, status)
+                self.alarm()
                 
 
     
     def run(self):
-        conn = sqlite3.connect(sys.path[0] + '/' + ctl.getGlobalVar('config')['Common']['DB']).cursor()
-        self._name, lowVar, highVar, mtd, alarmMode = self.getlVar(conn)
+        # conn = sqlite3.connect(sys.path[0] + '/' + ctl.getGlobalVar('config')['Common']['DB']).cursor()
+        # self._name, self._lowVar, self._highVar, self._mtd, self._alarmMode, self._useEmail, self._useBuz = self.getlVar(conn)
         # print(name, lowVar, highVar, mtd)
-        self.watchVar(self._name, lowVar, highVar, mtd, alarmMode)
+        self.watchVar(self._name)
         # while
